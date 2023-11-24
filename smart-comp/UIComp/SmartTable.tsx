20b923aff1e17bd88cd1bc4519a9db84dffd1a39
lx.jin <308561217@qq.com>
2023-11-24 11:09:26 +0800

    smartTable update rowKey

--- smart-comp/UIComp/SmartTable.tsx
+++ smart-comp/UIComp/SmartTable.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import type { ActionType, ProColumns } from '@ant-design/pro-components';
-import { ProTable, TableDropdown } from '@ant-design/pro-components';
-import { Space, Tag } from 'antd';
+import { ProTable } from '@ant-design/pro-components';
 import { getConfig } from '../Anotations/SmartTable';
 import SmartField from './SmartField';
 
@@ -21,124 +20,12 @@ type GithubIssueItem = {
     closed_at?: string;
 };
 
-// const columns: ProColumns<GithubIssueItem>[] = [
-//     {
-//         dataIndex: 'index',
-//         valueType: 'indexBorder',
-//         width: 48,
-//     },
-//     {
-//         title: '标题',
-//         dataIndex: 'title',
-//         copyable: true,
-//         ellipsis: true,
-//         tip: '标题过长会自动收缩',
-//         formItemProps: {
-//             rules: [
-//                 {
-//                     required: true,
-//                     message: '此项为必填项',
-//                 },
-//             ],
-//         },
-//     },
-//     {
-//         disable: true,
-//         title: '状态',
-//         dataIndex: 'state',
-//         filters: true,
-//         onFilter: true,
-//         ellipsis: true,
-//         valueType: 'select',
-//         valueEnum: {
-//             all: { text: '超长'.repeat(50) },
-//             open: {
-//                 text: '未解决',
-//                 status: 'Error',
-//             },
-//             closed: {
-//                 text: '已解决',
-//                 status: 'Success',
-//                 disabled: true,
-//             },
-//             processing: {
-//                 text: '解决中',
-//                 status: 'Processing',
-//             },
-//         },
-//     },
-//     {
-//         disable: true,
-//         title: '标签',
-//         dataIndex: 'labels',
-//         search: false,
-//         renderFormItem: (_, { defaultRender }) => {
-//             return defaultRender(_);
-//         },
-//         render: (_, record) => (
-//             <Space>
-//                 {record.labels.map(({ name, color }) => (
-//                     <Tag color={color} key={name}>
-//                         {name}
-//                     </Tag>
-//                 ))}
-//             </Space>
-//         ),
-//     },
-//     {
-//         title: '创建时间',
-//         key: 'showTime',
-//         dataIndex: 'created_at',
-//         valueType: 'date',
-//         sorter: true,
-//         hideInSearch: true,
-//     },
-//     {
-//         title: '创建时间',
-//         dataIndex: 'created_at',
-//         valueType: 'dateRange',
-//         hideInTable: true,
-//         search: {
-//             transform: (value) => {
-//                 return {
-//                     startTime: value[0],
-//                     endTime: value[1],
-//                 };
-//             },
-//         },
-//     },
-//     {
-//         title: '操作',
-//         valueType: 'option',
-//         key: 'option',
-//         render: (text, record, _, action) => [
-//             <a
-//                 key="editable"
-//                 onClick={() => {
-//                     action?.startEditable?.(record.id);
-//                 }}
-//             >
-//                 编辑
-//             </a>,
-//             <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
-//                 查看
-//             </a>,
-//             <TableDropdown
-//                 key="actionGroup"
-//                 onSelect={() => action?.reload()}
-//                 menus={[
-//                     { key: 'copy', name: '复制' },
-//                     { key: 'delete', name: '删除' },
-//                 ]}
-//             />,
-//         ],
-//     },
-// ];
-
 export default (props) => {
     const { entitySet } = props;
     const [currentState, setCurrentState] = useState<{ annoRequest: any }>()
     const [columns, setColumns] = useState<ProColumns<GithubIssueItem>[]>([]);
+    const actionRef = useRef<ActionType>();
+
     //初始化方法
     const init = async () => {
         const result = await getConfig({ entitySet })
@@ -170,7 +57,6 @@ export default (props) => {
         !currentState && init()
     }, [])
 
-    const actionRef = useRef<ActionType>();
     return (
         <ProTable<GithubIssueItem>
             columns={columns}
@@ -205,7 +91,7 @@ export default (props) => {
                     console.log('value: ', value);
                 },
             }}
-            rowKey="id"
+            rowKey="key"
             search={false}
             form={{
                 syncToUrl: (values, type) => {
