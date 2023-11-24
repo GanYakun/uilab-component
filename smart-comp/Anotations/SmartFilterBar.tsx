e5f9840db6bccd94ae7547f7ace15c798018175f
lx.jin <308561217@qq.com>
2023-11-24 19:13:28 +0800

    bug fixed

--- smart-comp/Anotations/SmartFilterBar.tsx
+++ smart-comp/Anotations/SmartFilterBar.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-19 14:59:09
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-24 19:09:16
+ * @LastEditTime: 2023-11-24 19:10:39
  * @FilePath: /uilab-gbms/lib/o3smart-comp/Anotations/SmartTable.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -44,9 +44,10 @@ const _getAnnoSelectionFields = async (entitySet, currentAnnotations) => {
             })
         })
     )
-    
+
     return result
 }
+
 const getConfig = async (params) => {
     const { entitySet } = params
     const { currentAnnotations } = await Utils.getEntitySetConfig(entitySet)
