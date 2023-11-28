e3aaed405eca8d258640a73a2adf3425f3d59e80
lx.jin <308561217@qq.com>
2023-11-28 09:05:57 +0800

    routes update

--- smart-comp/Anotations/ListReport.tsx
+++ smart-comp/Anotations/ListReport.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-19 14:59:09
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-27 17:17:26
+ * @LastEditTime: 2023-11-28 09:02:25
  * @FilePath: /uilab-gbms/lib/o3smart-comp/Anotations/SmartTable.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,7 +30,7 @@ const _getManifestConfig = async () => {
         if (routing && name === 'sap.fe.templates.ListReport') {
             const { routes, targets } = routing
             const targetRoute = targets[routeName]?.options?.settings?.navigation[entitySet]?.detail?.route
-            navigationRoute = `${window.location.pathname}/${targetRoute}`
+            navigationRoute = targetRoute
         }
         return {
             entitySet,
