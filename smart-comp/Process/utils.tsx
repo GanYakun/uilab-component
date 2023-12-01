039c8debbda3daca811e075a26df085cfc03fbbb
lx.jin <308561217@qq.com>
2023-12-01 18:36:54 +0800

    default filter bug fixed

--- smart-comp/Process/utils.tsx
+++ smart-comp/Process/utils.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-20 12:24:40
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-01 18:03:35
+ * @LastEditTime: 2023-12-01 18:36:10
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/Process/utils.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -1757,7 +1757,7 @@ const getSelectionVariantByAnnotations = (obj, currentEntityTypeData) => {
                                                                     Option = getTextValueByData('enumMember', h);
                                                                 }
                                                                 if (property === 'Low') {
-                                                                    Low = h[null] ? null : getTextValueByData('string', h);
+                                                                    Low = h[null] ? null : getTextValueByData('string', h) || getTextValueByData('bool', h);
                                                                 }
                                                                 // if (property === 'Sign') {
                                                                 //     Sign = getTextValueByData('enumMember', h);
