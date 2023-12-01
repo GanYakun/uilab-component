a807f3b4d2700c11f74103667b412fe789d3a77c
lx.jin <308561217@qq.com>
2023-12-01 16:26:42 +0800

    smartField anno update

--- smart-comp/Anotations/SmartField.tsx
+++ smart-comp/Anotations/SmartField.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-20 15:23:53
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-01 16:12:35
+ * @LastEditTime: 2023-12-01 16:25:22
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/Anotations/smartTable.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -269,6 +269,21 @@ const _setFieldValue = (currentAnnotations, currentPropertyType, isReadOnly, dat
         valueListConfig: null as any,
     }
 
+    //dataPoint
+    if (dataPoint) {
+        result.fieldType = 'DataPoint';
+        const { Visualization } = dataPoint
+        switch (Visualization) {
+            case 'UI.VisualizationType/Rating':
+                result.fieldType = 'Rating'
+                break;
+            case 'UI.VisualizationType/Progress':
+                result.fieldType = 'Progress'
+                break;
+        }
+        return result
+    }
+
     //只读返回
     if (isReadOnly) {
         result.fieldType = 'ReadOnly'
@@ -346,22 +361,6 @@ const _setFieldValue = (currentAnnotations, currentPropertyType, isReadOnly, dat
         result.fieldType = 'Hidden';
     }
 
-    //dataPoint
-    if (dataPoint) {
-        const { Visualization } = dataPoint
-        switch (Visualization) {
-            case 'UI.VisualizationType/Rating':
-                result.fieldType = 'Rating'
-                break;
-            case 'UI.VisualizationType/Progress':
-                result.fieldType = 'Progress'
-                break;
-            default:
-                result.fieldType = 'DataPoint';
-                break;
-        }
-    }
-
     return result
 }
 
@@ -431,7 +430,7 @@ export const getConfig = async (params) => {
     const unit = getUnit(currentAnnotations)
 
     //调试用
-    if (path === 'ddFormType') {
+    if (path === 'priority') {
         console.log('SmartField-Log', {
             path,
             currentPropertyType,
@@ -445,7 +444,8 @@ export const getConfig = async (params) => {
             nullable,
             action,
             defaultValue,
-            unit
+            unit,
+            dataPoint
         })
     }
     return {
--- smart-comp/UIPages/ObjectPage.tsx
+++ smart-comp/UIPages/ObjectPage.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-26 17:01:20
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-01 16:08:00
+ * @LastEditTime: 2023-12-01 16:19:00
  * @FilePath: /uilab-gbms/lib/o3smart-comp/UIPages/ListReport.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -85,13 +85,14 @@ export default (props) => {
                     }
 
                 case "UI.DataPoint":
-                    const { Title, Value } = value
+                    const { Title, Value,Criticality } = value
                     const DataPointTitle = Title ? Title : Value;
                     const option = {
                         dataPoint: value,
                         entitySet: currentState?.entitySet,
                         path: Value,
                         record: currentRecord,
+                        valueColor: Criticality
                     }
                     return {
                         type,
