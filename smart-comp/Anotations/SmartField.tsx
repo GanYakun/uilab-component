23983e80533aaea8be20c33b193e06fdc1805b24
lx.jin <308561217@qq.com>
2023-12-01 15:41:24 +0800

    smart add locale and bug fixed

--- smart-comp/Anotations/SmartField.tsx
+++ smart-comp/Anotations/SmartField.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-20 15:23:53
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-01 15:30:16
+ * @LastEditTime: 2023-12-01 15:36:53
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/Anotations/smartTable.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -335,7 +335,7 @@ const _setFieldValue = (currentAnnotations, currentPropertyType, isReadOnly) => 
         case 'Edm.Double':
             result.fieldType = 'Number'
             break;
-        case 'Edm.Stream"':
+        case 'Edm.Stream':
             result.fieldType = 'Upload'
             break;
         default:
---
+++ smart-comp/Process/locale.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: lx.jin 308561217@qq.com
+ * @Date: 2023-12-01 15:37:16
+ * @LastEditors: lx.jin 308561217@qq.com
+ * @LastEditTime: 2023-12-01 15:38:20
+ * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/Process/locale.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export const enUS={
+    //smart组件
+    'smart.true': 'Yes',
+    'smart.false': 'No'
+}
+export const zhCN = {
+    //smart组件
+   'smart.true': '是',
+   'smart.false': '否'
+}
