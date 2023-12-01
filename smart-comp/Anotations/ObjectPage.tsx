17d098a6db0a01803bfeac45dec117a239edb61d
lx.jin <308561217@qq.com>
2023-12-01 18:32:09 +0800

    getQueryArr update

--- smart-comp/Anotations/ObjectPage.tsx
+++ smart-comp/Anotations/ObjectPage.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-19 14:59:09
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-01 18:20:38
+ * @LastEditTime: 2023-12-01 18:27:52
  * @FilePath: /uilab-gbms/lib/o3smart-comp/Anotations/SmartTable.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -94,7 +94,9 @@ const getFieldArr = ({ HeaderInfo, Facets, HeaderFacets, HiddenPaths, Identifica
                         }
                     }
                 } else if (facetType === 'UI.DataPoint' && value) {
-                    result.push(value.Value)
+                    const { Value, Criticality } = value
+                    Value&&result.push(value.Value)
+                    Criticality && result.push(Criticality)
                 }
             }
         }
