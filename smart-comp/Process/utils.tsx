6de7300348fb3558f3289f1044f02674fde72468
lx.jin <308561217@qq.com>
2023-12-04 14:08:37 +0800

    add Action to Facets

--- smart-comp/Process/utils.tsx
+++ smart-comp/Process/utils.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-20 12:24:40
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-01 18:36:10
+ * @LastEditTime: 2023-12-04 14:04:04
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/Process/utils.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -1251,8 +1251,18 @@ const getTargetAnnotationProcessed = (
                         const record = Data[0]?.record
                         for (let c of record) {
                             const { type, propertyValue } = c
-                            const { Value, Criticality } = parsePropertyValue(propertyValue)
-                            Fields.push({ type, Value, Criticality })
+                            const { Value, Criticality, Action,Label } = parsePropertyValue(propertyValue)
+                            const obj = {
+                                type,
+                                Value,
+                                Criticality,
+                                Label
+                            } as any
+                            //处理Action
+                            if (type === 'UI.DataFieldForAction' && Action) {
+                                obj.Action = parseActionByName(Action)
+                            }
+                            Fields.push(obj)
                         }
                     }
                     return {
@@ -1953,7 +1963,7 @@ const parseActionByName = (actionName) => {
                         //message.error(< FormattedMessage id='smart.error' />);
                     })
                     .finally(() => {
-                        
+
                     });
             } else {
                 let option = {
