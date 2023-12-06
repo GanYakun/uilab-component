2dffff3b5ce3741e8adb9e4d0372d4b06ab6bc1c
lx.jin <308561217@qq.com>
2023-12-06 10:01:05 +0800

    steps update

--- smart-comp/CustComp/Steps/index.tsx
+++ smart-comp/CustComp/Steps/index.tsx
@@ -2,16 +2,21 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-12-06 08:09:21
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-06 08:41:59
+ * @LastEditTime: 2023-12-06 10:00:17
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/CustComp/Steps/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { Steps } from 'ant5';
 const description = 'This is a description.';
 import Odata from '../../../utils/odata/odata'
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 
-export default (props: any) => {
+/**
+ * @param {type} props.queryEntity 例子：SupplierParties('10602') 
+ * @param {type} props.isInline 是否内联 例子：true false
+ */
+export default (props) => {
+    const { queryEntity,isInline } = props
     const [currentRecord, setCurrentRecord] = useState()
 
     //查询item数据
@@ -27,11 +32,13 @@ export default (props: any) => {
     }
 
     useEffect(() => {
-        queryItems()
+        //queryItems()
+        console.log({ queryEntity, isInline })
     }, [])
 
     return (
         <Steps
+            type={isInline?'inline':'default'}
             current={1}
             items={[
                 {
