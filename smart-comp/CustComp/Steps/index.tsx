4e2362a5aa1e4a4faf3a29fd0ed085ebcfd646a6
lx.jin <308561217@qq.com>
2023-12-06 15:43:06 +0800

    steps stopPropagation

--- smart-comp/CustComp/Steps/index.tsx
+++ smart-comp/CustComp/Steps/index.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-12-06 08:09:21
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-06 11:02:40
+ * @LastEditTime: 2023-12-06 15:41:47
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/CustComp/Steps/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -34,6 +34,7 @@ export default (props) => {
                 items.push({
                     title: nodeName,
                     description: nodeDescription,
+                    disabled: true
                 })
                 if (isActive) {
                     current = index
@@ -47,16 +48,18 @@ export default (props) => {
     }
 
     useEffect(() => {
-        queryItems()
+        !currentRecord && queryItems()
     }, [])
 
     return (
-        <Steps
-            type={isInline ? 'inline' : 'default'}
-            current={currentRecord?.current}
-            items={currentRecord?.items}
-            percent={60}
-        />
+        <div onClick={(e)=>e.stopPropagation()}>
+            <Steps
+                type={isInline ? 'inline' : 'default'}
+                current={currentRecord?.current}
+                items={currentRecord?.items}
+                percent={60}
+            />
+        </div>
     )
 }
 
