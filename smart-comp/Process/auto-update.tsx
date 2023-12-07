d41c37bc9a7feb4217e80e2edf04af7236047858
lx.jin <308561217@qq.com>
2023-12-07 16:36:41 +0800

    dadd auto-update

--- smart-comp/Process/auto-update.tsx
+++ smart-comp/Process/auto-update.tsx
@@ -2,17 +2,15 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-12-07 15:04:13
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-07 16:17:32
+ * @LastEditTime: 2023-12-07 16:35:21
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/Process/auto-update.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import localStorage from 'localStorage';
 import { getLocale } from 'umi'
-let lastSrcs: any[] = JSON.parse(localStorage.getItem('lastSrcs'));
-const scriptReg = /\<script.*src=["'](?<src>[^"']+)/gm
-
 //获取最新页面中的script链接
 async function extractNewScripts() {
+    const scriptReg = /\<script.*src=["'](?<src>[^"']+)/gm
     const html = await fetch('/?_timestamp=' + Date.now()).then((resp) => resp.text())
     scriptReg.lastIndex = 0;
     let result = []
@@ -25,6 +23,7 @@ async function extractNewScripts() {
 
 //判断是否需要更新
 async function needUpdate() {
+    let lastSrcs: any[] = JSON.parse(localStorage.getItem('lastSrcs'));
     const newScripts = await extractNewScripts()
     console.log({ newScripts, lastSrcs, getLocale: getLocale() })
     if (!lastSrcs) {
@@ -54,7 +53,7 @@ function autoRefresh() {
     setTimeout(async () => {
         const willUpdate = await needUpdate()
         if (willUpdate) {
-            const result = confirm(getLocale() === 'en' ? 'New version detected, do you want to update it?' : '检测到新版本，是否更新?')
+            const result = confirm(getLocale().search('en') !== -1 ? 'New version detected, do you want to update it?' : '检测到新版本，是否更新?')
             if (result) {
                 localStorage.removeItem('lastSrcs')
                 location.reload()
