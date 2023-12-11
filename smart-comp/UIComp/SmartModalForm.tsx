8e59900a9a0d8b991f0fd1c143eab045393ff434
lx.jin <308561217@qq.com>
2023-12-11 10:36:23 +0800

    button add ghost

--- smart-comp/UIComp/SmartModalForm.tsx
+++ smart-comp/UIComp/SmartModalForm.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-28 14:12:49
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-08 17:33:58
+ * @LastEditTime: 2023-12-11 10:23:42
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/UIComp/SmartModalForm.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -67,7 +67,7 @@ export default (props: { entitySet: string; content: any; onSubmit: any; fields:
             layout='vertical'
             title={title}
             trigger={
-                <Button type={btnType ? btnType : 'primary'}>
+                <Button type={btnType ? btnType : 'primary'} ghost>
                     {btnText}
                 </Button>
             }
