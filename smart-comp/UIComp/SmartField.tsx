196282c0e302e426b4eec4a9ccb79fe3681f2261
lx.jin <308561217@qq.com>
2023-12-13 19:03:49 +0800

    iconUrl bug fixed

--- smart-comp/UIComp/SmartField.tsx
+++ smart-comp/UIComp/SmartField.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-26 17:01:20
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-13 18:45:56
+ * @LastEditTime: 2023-12-13 19:02:52
  * @FilePath: /uilab-gbms/lib/o3smart-comp/UIPages/ListReport.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -410,7 +410,7 @@ export default (props: any) => {
                             onClick={(e) => e.stopPropagation()}
                             href={urlValue}
                             target="_blank"
-                            style={{ display: 'flex', alignItems: 'center' }}
+                            style={{ display: 'flex', alignItems: 'center', width: 30 }}
                         >
                             <Icon name={IconUrl} size={30} />
                         </a>
