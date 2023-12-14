cc84fa1ec58153664e9fc6aae32f47bbf5e2e563
lx.jin <308561217@qq.com>
2023-12-14 11:12:03 +0800

    download update

--- smart-comp/UIComp/SmartField.tsx
+++ smart-comp/UIComp/SmartField.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-26 17:01:20
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-13 19:02:52
+ * @LastEditTime: 2023-12-14 11:11:06
  * @FilePath: /uilab-gbms/lib/o3smart-comp/UIPages/ListReport.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -403,28 +403,31 @@ export default (props: any) => {
                 }
 
                 //判断是否有图标
-                if (IconUrl) {
+                if (pathValue) {
+                    if (IconUrl) {
+                        return (
+                            <a
+                                onClick={(e) => e.stopPropagation()}
+                                href={urlValue}
+                                target="_blank"
+                                style={{ display: 'flex', alignItems: 'center', width: 30 }}
+                            >
+                                <Icon name={IconUrl} size={30} />
+                            </a>
+                        )
+                    }
                     return (
                         <a
                             onClick={(e) => e.stopPropagation()}
                             href={urlValue}
                             target="_blank"
-                            style={{ display: 'flex', alignItems: 'center', width: 30 }}
                         >
-                            <Icon name={IconUrl} size={30} />
+                            {pathValue}
                         </a>
                     )
                 }
-                return (
-                    <a
-                        onClick={(e) => e.stopPropagation()}
-                        href={urlValue}
-                        target="_blank"
-                    >
-                        {pathValue}
-                    </a>
-                )
-            case 'Rating':
+                return <></>
+        case 'Rating':
                 return (
                     <Rate
                         defaultValue={record[path]}
