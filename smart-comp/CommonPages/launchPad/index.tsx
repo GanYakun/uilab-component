5f121346e5cb57e8f3642f18f4c9fee9e39c0cda
lx.jin <308561217@qq.com>
2023-12-14 11:25:44 +0800

    launchPad update

--- smart-comp/CommonPages/launchPad/index.tsx
+++ smart-comp/CommonPages/launchPad/index.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-08-04 18:10:09
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-13 15:19:38
+ * @LastEditTime: 2023-12-14 11:16:55
  * @FilePath: /qiankun/uiLab/apps/launchPad/src/pages/LaunchPad.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -52,7 +52,7 @@ const LaunchPad: React.FC = () => {
     'supplierparty-manage': <Statistic title="Registered" value={112} prefix={<FileDoneOutlined />} valueStyle={{ color: '#3f8600' }} />,
     'supplierapprove-managebyapplication': <Progress type="circle" percent={30} size={60} />,
     'supplierapprove-managebyprocurement': <Statistic title="To be registered" value={47} prefix={<FileSyncOutlined />} valueStyle={{ color: '#00BFFF' }} />,
-    'supplierapprove-managebycompliance': <Progress percent={50} />,
+    'supplierapprove-managebycompliance': <Statistic title="To be registered" value={12} prefix={<FileSyncOutlined />} valueStyle={{ color: '#00BFFF' }} />,
   }
 
   return <div>
