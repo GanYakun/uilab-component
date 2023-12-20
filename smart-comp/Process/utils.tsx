7bd6e1ebedc9772359a4268f62383c56085762aa
lx.jin <308561217@qq.com>
2023-12-20 12:23:02 +0800

    utils update

--- smart-comp/Process/utils.tsx
+++ smart-comp/Process/utils.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-20 12:24:40
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-20 11:40:26
+ * @LastEditTime: 2023-12-20 12:22:19
  * @FilePath: /Uilab-Application/lib/Uilab-Comp/smart-comp/Process/utils.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -800,10 +800,9 @@ const getFieldDisplayValueAndCurrentValue = (
                 currentValue = value2;
             }
         } else {
-            console.log({ record, fieldValue })
-            // currentPathText = record
-            // displayValue = record;
-            // currentValue = record;
+            currentPathText = record
+            displayValue = record;
+            currentValue = record;
         }
     }
 
