a29c6a42f857f825d52756b2a72af1217dc517a6
lx.jin <308561217@qq.com>
2023-12-22 18:26:53 +0800

    bug fixed

--- smart-comp/UIPages/ObjectPage.tsx
+++ smart-comp/UIPages/ObjectPage.tsx
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-26 17:01:20
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-22 17:29:50
+ * @LastEditTime: 2023-12-22 18:26:24
  * @FilePath: /uilab-gbms/lib/o3smart-comp/UIPages/ListReport.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -443,6 +443,7 @@ export default (props: any) => {
                 entitySet: entitySet,
                 path: Title.Value,
                 record: currentRecord,
+                showLabel:false
             }
 
             const subTitleOption = {
@@ -450,6 +451,7 @@ export default (props: any) => {
                 entitySet: entitySet,
                 path: Description?.Value,
                 record: currentRecord,
+                showLabel: false
             }
 
             const extra = []
